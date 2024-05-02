d9b23a7b2d9f92312ab071542605bc47347d7114
Allan Ramos <allangrds@gmail.com>
2024-05-02 17:14:01 -0300

    fix(dashboard): changing menu toggle on mobile

--- admin-app/src/components/dashboard-wrapper/dashboard-wrapper.tsx
+++ admin-app/src/components/dashboard-wrapper/dashboard-wrapper.tsx
@@ -34,7 +34,9 @@ export const highlightActiveMenuItem = (checkPath: string): boolean => {
 export const DashboardWrapper = ({
   breadcrumbs, children, description, title,
 }: Props) => {
+  const menuRef = React.useRef<HTMLDivElement>(null)
   const [isDarkMode, setIsDarkMode] = React.useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false)
   const navigate = useNavigate()
 
   const handleOnChangeTheme = () => {
@@ -46,6 +48,22 @@ export const DashboardWrapper = ({
     LocalStorage.removeAccessToken()
     navigate(routes.login.path)
   }
+
+  const handleToggleSidebar = () => setIsSidebarOpen((prev) => !prev)
+
+  React.useEffect(() => {
+    const closeSidebarOnOutsideClick = (event: any) => {
+      if (isSidebarOpen && !menuRef.current?.contains(event.target as Node)) {
+        setIsSidebarOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', closeSidebarOnOutsideClick)
+
+    return () => {
+      document.removeEventListener('mousedown', closeSidebarOnOutsideClick)
+    }
+  }, [isSidebarOpen])
 
   return (
     <>
@@ -72,7 +90,12 @@ export const DashboardWrapper = ({
       </header>
       <div className="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
         <div className="flex items-center py-4">
-          <button type="button" className="text-gray-500 hover:text-gray-600" data-hs-overlay="#application-sidebar" aria-controls="application-sidebar" aria-label="Toggle navigation">
+          <button
+            type="button"
+            className="text-gray-500 hover:text-gray-600"
+            aria-label="Toggle navigation"
+            onClick={handleToggleSidebar}
+          >
             <span className="sr-only">Diminuir navegação</span>
             <svg className="size-5" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -112,7 +135,10 @@ export const DashboardWrapper = ({
         </div>
       </div>
 
-      <div id="application-sidebar" className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
+      <div
+        ref={menuRef}
+        className={`${isSidebarOpen ? '' : 'hidden'} hs-overlay hs-overlay-open:translate-x-0 transform fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700`}
+      >
         <div className="px-6">
           <p className="flex-none text-xl font-semibold text-gray-800 dark:text-white">
             TravelersApp
